6f71fad6e88f532506d2489926f4d7f54eb0d5e6
John Idogun <sirneij@gmail.com>
2026-07-18 10:26:36 -0700

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		experimental: {
+			remoteFunctions: true
+		}
+	},
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
+	}
+};
+
+export default config;
